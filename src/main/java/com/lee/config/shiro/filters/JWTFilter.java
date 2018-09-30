a797f8189025344c4eac6216969efab218dc8c88
liwenliang <308374811@qq.com>
2018-09-30 15:04:59 +0800

    jwtFilter优化

--- src/main/java/com/lee/config/shiro/filters/JWTFilter.java
+++ src/main/java/com/lee/config/shiro/filters/JWTFilter.java
@@ -23,18 +23,11 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
     private final static Logger logger = LoggerFactory.getLogger(JWTFilter.class);
 
     /**
-     * 判断用户是否想要登入。
-     * 检测header里面是否包含Authorization字段即可
-     */
-    @Override
-    protected boolean isLoginAttempt(ServletRequest request, ServletResponse response) {
-        HttpServletRequest req = (HttpServletRequest) request;
-        String authorization = req.getHeader("token");
-        return authorization != null;
-    }
-
-    /**
-     *
+     * 进入shiro认证
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
      */
     @Override
     protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
@@ -50,22 +43,18 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
     }
 
     /**
-     * 这里我们详细说明下为什么最终返回的都是true，即允许访问
-     * 例如我们提供一个地址 GET /article
-     * 登入用户和游客看到的内容是不同的
-     * 如果在这里返回了false，请求会被直接拦截，用户看不到任何东西
-     * 所以我们在这里返回true，Controller中可以通过 subject.isAuthenticated() 来判断用户是否登入
-     * 如果有些资源只有登入用户才能访问，我们只需要在方法上面加上 @RequiresAuthentication 注解即可
-     * 但是这样做有一个缺点，就是不能够对GET,POST等请求进行分别过滤鉴权(因为我们重写了官方的方法)，但实际上对应用影响不大
+     * 都进行认证
+     * @param request
+     * @param response
+     * @param mappedValue
+     * @return
      */
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
-        if (isLoginAttempt(request, response)) {
-            try {
-                executeLogin(request, response);
-            } catch (Exception e) {
-                printRes((HttpServletResponse) response, "missing or invalid token");
-            }
+        try {
+            executeLogin(request, response);
+        } catch (Exception e) {
+            printRes((HttpServletResponse) response, "missing or invalid token");
         }
         return true;
     }
